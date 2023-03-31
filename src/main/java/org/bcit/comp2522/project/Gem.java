f1ec90483b8440a41de0c4ac1d517b8ed2704f88
noorksangha <113377440+noorksangha@users.noreply.github.com>
2023-03-31 14:29:17 -0700

    Update Gem.java

--- src/main/java/org/bcit/comp2522/project/Gem.java
+++ src/main/java/org/bcit/comp2522/project/Gem.java
@@ -1,51 +1,49 @@
 package org.bcit.comp2522.project;
 
 import processing.core.PApplet;
-import processing.core.PImage;
 
-public class Gem extends PApplet{
-    private float x;
+public class Gem {
+  private float x;
 
-    private float y;
-    private int width;
 
-    private int height;
-    private float velocityX;
 
-    PImage gemImage;
+  private float y;
+  private int width;
 
-    public Gem(float x, float y, int width, int height, float velocityX) {
-        this.x = x;
-        this.y = y;
-        this.width = 100;
-        this.height = 200;
-        this.velocityX = velocityX;
-    }
+  private int height;
+  private float velocityX;
 
-    public void update() {
-        x += velocityX;
-    }
+  public Gem(float x, float y, int width, int height, float velocityX){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.velocityX = velocityX;
+  }
 
-    public void render(PApplet p) {
-        PImage gemImage = p.loadImage("gem.png");
-        p.image(gemImage, x, y, width, height);
-    }
+  public void update(){
+    x += velocityX;
+  }
+  public void render(PApplet p){
+    p.fill(180,0,0);
+    p.rect(x,y,width,height);
+  }
 
-    public float getY() {
-        return y;
-    }
+  public float getY() {
+    return y;
+  }
 
-    public float getX() {
-        return x;
-    }
+  public float getX() {
+    return x;
+  }
 
-    public float getWidth() {
-        return x;
-    }
+  public float getWidth() {
+    return x;
+  }
 
-    public float getHeight() {
-        return x;
-    }
+  public float getHeight() {
+    return x;
+  }
 
 
 }
